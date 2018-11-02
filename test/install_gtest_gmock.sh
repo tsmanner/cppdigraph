1c112cfe3f31e82f77f7c66c22541608c7c97d12
Tom Manner <tsmanner@us.ibm.com>
2018-11-02 14:17:06 -0400

    Updated Travis to use `make install` for gtest and gmock, and to install `dot`

--- test/install_gtest_gmock.sh
+++ test/install_gtest_gmock.sh
@@ -2,15 +2,9 @@
 
 wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz -O googletest.tar.gz
 tar -zxvf googletest.tar.gz
-cd googletest-release-1.8.0/googletest
-sudo cmake .
+mkdir googletest-release-1.8.0/build
+cd googletest-release-1.8.0/build
+sudo cmake ..
 sudo make
-sudo mv libg* /usr/lib/
-sudo cp include/gtest/ /usr/include/ -rf
-cd -
-cd googletest-release-1.8.0/googlemock
-sudo cmake .
-sudo make
-sudo mv libg* /usr/lib/
-sudo cp include/gmock/ /usr/include/ -rf
+sudo make install
 cd -
